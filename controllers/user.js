a4d974f859d7212573b198ccae424d049b7846b5
Andrew <andrewpeliza@hotmail.com>
2020-02-11 18:57:04 +0000

    Fixed typo in user controller, now we can create users.

--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,41 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 exports.getAllUsers = (req, res) => {
-  User.find({}, (err, user) => {
+  User.find({}, (err, users) => {
     if (err) {
       return res.status(500).json({
         message: 'Users Not Found!'
       });
     }
     console.log('Users has been requested.');
-    return res.json(user);
+    return res.json(users);
   });
+};
+
+exports.createUser = (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address,
+    phoneNumber: req.body.phoneNumber,
+    orders: req.body.order
+  });
+  user
+    .save()
+    .then(createdUser => {
+      console.log(`created user - ${createdUser}`);
+      res.status(201).json({
+        message: 'User added Successfully',
+        user: {
+          ...createdUser,
+          id: createdUser._id
+        }
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Creating User Failed!'
+      });
+    });
 };
--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,6 @@ const UserController = require('../controllers/user');
 
 router.get('', UserController.getAllUsers);
 
+router.post('', UserController.createUser);
+
 module.exports = router;
